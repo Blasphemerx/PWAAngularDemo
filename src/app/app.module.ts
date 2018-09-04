857391f72d6e481c54fc555570e9456158f93341
Ray.Xu <blasphemerx@outlook.com>
2018-09-04 11:57:40 +0800

    Upload project to github

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {MatToolbarModule, MatCardModule, MatButtonModule} from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { ImgCardComponent } from './img-card/img-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImgCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
